697c8a23950a0bccaa8d8802f8af90ba5ea99b6f
LorenzM97 <lo.mindner@gmx.de>
2018-08-15 15:44:18 +0200

    New Class

--- SolarSystem/ClassLibrary/MovingSpaceObject.cs
+++ SolarSystem/ClassLibrary/MovingSpaceObject.cs
@@ -10,7 +10,27 @@ namespace ClassLibrary
         public int Y;
         public int Size;
         public int Distance;
+        public string Name, Type;
         List<MovingSpaceObject> _childMoons = new List<MovingSpaceObject>();
 
+        public MovingSpaceObject(string type, string name, int x, int y, int size, int distance)
+        {
+            Type = type;
+            X = x;
+            Y = y;
+            Size = size;
+            Distance = distance;
+            Name = name;
+        }
+
+        public MovingSpaceObject(string type, string name, int x, int y, int size)
+        {
+            Type = type;
+            X = x;
+            Y = y;
+            Size = size;
+            Distance = 0;
+            Name = name;
+        }
     }
 }
---
+++ SolarSystem/ClassLibrary/Solarsystem.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Solarsystem
+{
+    public class Solarsystem
+    {
+        public string Name;
+        List<MovingSpaceObject> _childPlanets = new List<MovingSpaceObject>();
+
+        public Solarsystem(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
