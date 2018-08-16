504b0ecc915ed480be1ed54faeb08edf10728d9e
LorenzM97 <lo.mindner@gmx.de>
2018-08-16 13:27:56 +0200

    valuescontroller

--- SolarSystem/WPF/MainWindow.xaml.cs
+++ SolarSystem/WPF/MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using Library_Solarsystem;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,12 +26,55 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int count;
-        char[] numberCheck;
-        List<Solarsystem> _lstSolarSystems = new List<Solarsystem>();
+
+        public ObservableCollection<string> Solarsystems;
+
         public MainWindow()
         {
+            Solarsystems = new ObservableCollection<string>();
+
             InitializeComponent();
+
+            combo.DataContext = Solarsystems;
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var c = new HttpClient() {  })
+            {
+                HttpResponseMessage response = await c.GetAsync(new Uri(@"http://localhost:59306/api/values"));
+                if (response.IsSuccessStatusCode)
+                {
+                    foreach (var item in await response.Content.ReadAsAsync<string[]>())
+                    {
+                        if(!Solarsystems.Contains(item))
+                            Solarsystems.Add(item);
+                    }
+                    
+                }
+
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Grid_Loaded(null, null);
+        }
+
+        private async void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (var c = new HttpClient() { })
+            {
+                HttpResponseMessage response = await c.GetAsync(new Uri($"http://localhost:59306/api/values/{combo.SelectedValue}"));
+                if (response.IsSuccessStatusCode)
+                {
+                    this.DataContext = await response.Content.ReadAsAsync<SpaceObject>();
+
+                }
+
+
+            }
         }
     }
 }
