e2f2d6296cd3c6f04d0348409da4560f4277577b
LorenzM97 <lo.mindner@gmx.de>
2018-08-21 15:44:08 +0200

    Added Monogame for second time... #shit

---
+++ SolarSystem/MonogameNew/WidthHeight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame
+{
+    class WidthHeight
+    {
+        public static int screenWidth { get;  set; }
+        public static int screenHight { get;  set; }
+    
+    }
+}
+    
